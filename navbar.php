953c894d21f5a48b67b5eb7be3afa4a1fed55dc7
Angelo Alindayo <angeloalindayo.151306@gmail.com>
2026-06-19 10:01:08 +0800

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,10 @@
+<div class="navbar">
+    <div class="nav-left">
+        <h1>Inventory System</h1>
+    </div>
+    <div class="nav-right">
+        <a href="index.php">Products</a>
+        <a href="report.php">Reports</a>
+        <a href="add.php">Add Product</a>
+    </div>
+</div>
